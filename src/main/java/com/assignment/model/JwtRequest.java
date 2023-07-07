11018333b8f8fd793289579d484cd2c7b83c6e62
Gaurav <Gaurav@DESKTOP-I9I7S3Q>
2023-07-07 23:14:58 +0530

    initial

---
+++ src/main/java/com/assignment/model/JwtRequest.java
@@ -0,0 +1,24 @@
+package com.assignment.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class JwtRequest {
+	private String email;
+	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+   
+}
---
+++ src/main/java/com/assignment/model/JwtResponse.java
@@ -0,0 +1,30 @@
+package com.assignment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtResponse {
+	private String jwtToken;
+	private String username;
+	public String getJwtToken() {
+		return jwtToken;
+	}
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
